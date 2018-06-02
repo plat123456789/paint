be322d9ac5f11d96ccc9b5efb0178e44379dc603
plat123456789 <plat123456789@qq.com>
2018-06-03 05:27:43 +0800

    add some stupid effects & make all the photo un-draggable & un-selectable

--- js/buttons.js
+++ js/buttons.js
@@ -1,6 +1,5 @@
-
-
 //the below 4 are for line, shape, stroke & action button, they all have sub menu
+//shape
 $('#shape').click(() => {
     $(".shape-sub-menu").toggle();
 
@@ -16,6 +15,7 @@ $('#shape').click(() => {
     }
 });
 
+//stroke
 $('#stroke').click(() => {
     $(".stroke-sub-menu").toggle();
 
@@ -31,6 +31,7 @@ $('#stroke').click(() => {
     }
 });
 
+//action-do
 $('#action-do').click(() => {
     $(".action-do-sub-menu").toggle();
 
@@ -46,6 +47,7 @@ $('#action-do').click(() => {
     }
 });
 
+//line
 $('#line').click(() => {
     $(".line-sub-menu").toggle();
 
@@ -62,14 +64,79 @@ $('#line').click(() => {
 });
 
 //buttons without sub-menu
+//brush
 $('#brush').click(() => {
     currentFunction = new DrawingPencil(contextReal, contextDraft);
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
+
+//eraser
 $('#eraser').click(() => {
     currentFunction = new DrawingEraser(contextReal, contextDraft);
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
+
+//spray
 $("#spray").click(() => {
     currentFunction = new DrawingSpray(contextReal);
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
 
 
@@ -78,46 +145,208 @@ $("#spray").click(() => {
 $("#straight-line").click(() => {
     $(".line-sub-menu").toggle();
     currentFunction = new DrawingSTline(contextReal, contextDraft);
-    $("#line-img").attr("src", "painter-icon2/png/line.png");
+
+    //change side-bar icon to the selected function icon
+    $("#line-img").fadeOut(500, function() {
+        $("#line-img").attr("src","painter-icon2/png/line.png");
+        $("#line-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
+
+//line sub menu
 $("#curve").click(() => {
     $(".line-sub-menu").toggle();
     currentFunction = new DrawingCurve(contextReal, contextDraft);
-    $("#line-img").attr("src", "painter-icon2/png/curved-line.png");
+
+    //change side-bar icon to the selected function icon
+    $("#line-img").fadeOut(500, function() {
+        $("#line-img").attr("src","painter-icon2/png/curved-line.png");
+        $("#line-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
 
 //the below 3 are the shape sub menu
 $("#circle").click(() => {
     $(".shape-sub-menu").toggle();
     currentFunction = new DrawingCircle(contextReal, contextDraft);
-    $("#shape-img").attr("src", "painter-icon2/png/circle.png");
+
+    //change side-bar icon to the selected function icon
+    $("#shape-img").fadeOut(500, function() {
+        $("#shape-img").attr("src","painter-icon2/png/circle.png");
+        $("#shape-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
+
+//shape sub menu
 $("#square").click(() => {
     $(".shape-sub-menu").toggle();
     currentFunction = new DrawingRectangle(contextReal, contextDraft);
-    $("#shape-img").attr("src", "painter-icon2/png/square.png");
+
+    //change side-bar icon to the selected function icon
+    $("#shape-img").fadeOut(500, function() {
+        $("#shape-img").attr("src","painter-icon2/png/square.png");
+        $("#shape-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
+
+//shape sub menu
 $("#triangle").click(() => {
     $(".shape-sub-menu").toggle();
     currentFunction = new DrawingTriangle(contextReal, contextDraft);
-    $("#shape-img").attr("src", "painter-icon2/png/triangle.png");
+
+    //change side-bar icon to the selected function icon
+    $("#shape-img").fadeOut(500, function() {
+        $("#shape-img").attr("src","painter-icon2/png/triangle.png");
+        $("#shape-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#stroke-img").attr("src")!=="painter-icon2/png/selection.png"){
+        $("#stroke-img").fadeOut(500, function() {
+            $("#stroke-img").attr("src","painter-icon2/png/selection.png");
+            $("#stroke-img").fadeIn(500);
+        });
+    }
 });
 
 //the below 3 are the stroke sub menu
 $("#circle-stroke").click(() => {
     $(".stroke-sub-menu").toggle();
     currentFunction = new DrawingCircleStroke(contextReal, contextDraft);
-    $("#stroke-img").attr("src", "painter-icon2/png/circle-stroke.png");
+
+    //change side-bar icon to the selected function icon
+    $("#stroke-img").fadeOut(500, function() {
+        $("#stroke-img").attr("src","painter-icon2/png/circle-stroke.png");
+        $("#stroke-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
 });
+
+//stroke sub menu
 $("#square-stroke").click(() => {
     $(".stroke-sub-menu").toggle();
     currentFunction = new DrawingRectangleStroke(contextReal, contextDraft);
-    $("#stroke-img").attr("src", "painter-icon2/png/rectangle-stroke.png");
+
+    //change side-bar icon to the selected function icon
+    $("#stroke-img").fadeOut(500, function() {
+        $("#stroke-img").attr("src","painter-icon2/png/rectangle-stroke.png");
+        $("#stroke-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
 });
+
+//stroke sub menu
 $("#triangle-stroke").click(() => {
     $(".stroke-sub-menu").toggle();
     currentFunction = new DrawingTriangleStroke(contextReal, contextDraft);
-    $("#stroke-img").attr("src", "painter-icon2/png/triangle-stroke.png");
+    
+    //change side-bar icon to the selected function icon
+    $("#stroke-img").fadeOut(500, function() {
+        $("#stroke-img").attr("src","painter-icon2/png/triangle-stroke.png");
+        $("#stroke-img").fadeIn(500);
+    });
+
+    //change back the main icon photo when other drawing function was clicked;
+    if($("#line-img").attr("src")!=="painter-icon2/png/vector.png"){
+        $("#line-img").fadeOut(500, function() {
+            $("#line-img").attr("src","painter-icon2/png/vector.png");
+            $("#line-img").fadeIn(500);
+        });
+    }
+    if($("#shape-img").attr("src")!=="painter-icon2/png/033-ruler-1.png"){
+        $("#shape-img").fadeOut(500, function() {
+            $("#shape-img").attr("src","painter-icon2/png/033-ruler-1.png");
+            $("#shape-img").fadeIn(500);
+        });
+    }
 });
 
 
@@ -128,12 +357,16 @@ $("#undo").click(() => {
         trialSetting.process.undoFunction();
     }
 });
+
+//action-do-sub menu
 $("#redo").click(() => {
     event.preventDefault();
     if(trialSetting.process.currentIndex>=0&&trialSetting.process.currentIndex<trialSetting.process.arrayImg.length-1){
         trialSetting.process.redoFunction();
     }
 });
+
+//action-do-sub menu
 $("#clear").click(() => {
     if(confirm("Are you sure")){
     }
@@ -141,12 +374,15 @@ $("#clear").click(() => {
         event.preventDefault();
     }
 });
+
+//action-do-sub menu
 $("#save").click(() => {
     localStorage.setItem("canvasName", canvasReal.toDataURL());
     alert("Your Drawings are Saved!")
     $(".action-do-sub-menu").toggle();
 });
 
+//action-do-sub menu
 $("#load").click(() => {
     var loadImg = confirm("The saved Drawings will be Loaded!")
     if (loadImg){
@@ -161,6 +397,7 @@ $("#load").click(() => {
     $(".action-do-sub-menu").toggle();
 });
 
+//action-do-sub menu
 $("#download").click(() => {
     let downloadImg = canvasReal.toDataURL("image/png").replace("image/png", "image/octet-stream");
     document.location.href = downloadImg;
@@ -170,7 +407,7 @@ $("#download").click(() => {
 
 
 
-//close all sub-menu when you click the canvas
+//close all sub-menu when you click the canvas-draft
 $("#canvas-draft").click(() => {
     if($(".shape-sub-menu").is(":visible")){
         $(".shape-sub-menu").toggle();
